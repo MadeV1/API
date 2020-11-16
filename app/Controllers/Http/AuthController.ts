78c2261284d0312a813b1497156ba6b78434d547
Alexandre Gérault <alexandre@hexium.io>
2020-11-17 00:29:06 +0100

    refactor(registration): use validator class

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,20 +1,10 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import SecurityUser from 'App/Models/SecurityUser'
+import RegistrationValidator from 'App/Validators/RegistrationValidator'
 
 export default class AuthController {
   public async register({ request, response }: HttpContextContract) {
-    const validationSchema = schema.create({
-      pseudonym: schema.string({ trim: true }, [rules.minLength(3), rules.maxLength(30)]),
-      email: schema.string({ trim: true, escape: true }, [
-        rules.email(),
-        rules.unique({ table: 'security_users', column: 'email' }),
-        rules.maxLength(255),
-      ]),
-      password: schema.string({ trim: true }, [rules.confirmed(), rules.minLength(6)]),
-    })
-
-    const userDetails = await request.validate({ schema: validationSchema })
+    const userDetails = await request.validate(RegistrationValidator)
 
     const user: SecurityUser = new SecurityUser()
     user.pseudonym = userDetails.pseudonym
