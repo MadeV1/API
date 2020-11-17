3dab8d9150f0ff63db253396953ac935faf3a903
Alexandre Gérault <alexandre@hexium.io>
2020-11-17 22:04:49 +0100

    feat(registration): add uniqueness tests

--- app/Validators/RegistrationValidator.ts
+++ app/Validators/RegistrationValidator.ts
@@ -5,7 +5,11 @@ export default class RegistrationValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    pseudonym: schema.string({ trim: true }, [rules.minLength(3), rules.maxLength(30)]),
+    pseudonym: schema.string({ trim: true }, [
+      rules.minLength(3),
+      rules.maxLength(30),
+      rules.unique({ table: 'security_users', column: 'pseudonym' }),
+    ]),
     email: schema.string({ trim: true, escape: true }, [
       rules.email(),
       rules.unique({ table: 'security_users', column: 'email' }),
