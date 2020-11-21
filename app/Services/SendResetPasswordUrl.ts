1548f99db45f4ec7b09796d451c670802fa2103f
Alexandre Gérault <alexandre@hexium.io>
2020-11-21 11:06:06 +0100

    feat(reset-password): service to send mail containing signed url

---
+++ app/Services/SendResetPasswordUrl.ts
@@ -0,0 +1,23 @@
+import Route from '@ioc:Adonis/Core/Route'
+import Mail from '@ioc:Adonis/Addons/Mail'
+import SecurityUser from 'App/Models/SecurityUser'
+
+export default class SendResetPasswordUrl {
+  public static async send(user: SecurityUser) {
+    const signedUrl = Route.makeSignedUrl('/password/reset', {
+      params: {
+        email: user.email,
+      },
+    })
+
+    await Mail.send((message) => {
+      message
+        .from('message@hey.com')
+        .to(user.email)
+        .subject('Réinitilisation du mot de passe')
+        .textView('password/reset', {
+          url: process.env.FRONTEND_BASE_URL! + signedUrl,
+        })
+    })
+  }
+}
